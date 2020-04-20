427464939f426f2de8a6642940d313ec41633b67
Jae Hwan Jeong <jeong7331@Jaes-MacBook-Pro-2.local>
2020-04-21 02:09:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+Packages
+
+
+Numpy
+pip install numpy
+v. 1.18.3 
+
+Pandas 
+pip install pandas
+v. 1.0.3
---
+++ article_data.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+print("in article parsing")
