726c9577aff803e911d4ae2b582c4fce3d448d5d
hyrumk <60143710+hyrumk@users.noreply.github.com>
2020-06-14 21:03:10 +0900

    Add files via upload
    Added functions

--- text_processing.py
+++ text_processing.py
@@ -5,26 +5,128 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as sia
 from nltk.tokenize import word_tokenize, sent_tokenize
+import string
 import random
 import matplotlib.pyplot as plt
 
+
+'''
+Loading a Text Data.
+'''
 # a list of news in a string form for each day.
 #text = data_collector.load_data('apple', 'keyword')
-text = data_collector.load_data('apple_technews', 'keyword')
-
+#text = data_collector.load_data('apple_technews', 'keyword')
+text = data_collector.load_data('apples', 'gfkeyword')
 
 #for txt in text:
 #    if len(txt) > 1:
 #        print(txt)
 #text = data_collector.title_from_url_list('apple')
 
-
+'''
+Selecting a Type of Label
+'''
 #label = data_collector.load_data('AAPL', 'label')
-#label = stock_data.stock_price_label('AAPL', interval = 1, percentage_rate = 1)
+#label = stock_data.stock_price_label('AAPL', interval = 3, percentage_rate = 1) # company mentioned polarity correlates.
+#label = stock_data.stock_price_label2('AAPL', interval = 1, consecutive = 3)
+label = stock_data.stock_price_label3('AAPL', 'IXIC', 1, percentage_rate = 3, start_date = '2010-01-01')
 #label = stock_data.stock_price_label_binary('AAPL', 10) #'AAPL',14,5
-#label = stock_data.stock_price_label_binary('AAPL', 7, '2010-01-01')
-label = stock_data.stock_price_label_binary2('AAPL', 'IXIC', 1 , '2010-01-01')
-#label = stock_data.stock_price_label2('AAPL', interval = 1, consecutive = 5)
+#label = stock_data.stock_price_label_binary('AAPL', 1, '2010-01-01')
+#label = stock_data.stock_price_label_binary2('AAPL', 'IXIC', 1 , '2010-01-01')
+
+
+
+#growth_rate = stock_data.market_stock_growth_interval('AAPL', 'IXIC', 14, '2010-01-01') # for feature vs rate
+#merged_data = data_collector.merge_price_text(text, growth_rate) # for feature vs rate #(text, label)
+merged_data = data_collector.merge_price_text(text, label)
+
+
+
+def addup_fd(total_fd, fd_to_add):
+    '''
+    This function is for the function 'extract_related_keyword'.
+
+    :param total_fd: the fd to be added to.
+    :param fd_to_add: the fd to add.
+    :return: each element of the fd_to_add added to total_fd
+    '''
+    for word in fd_to_add:
+        if word in total_fd:
+            total_fd[word] += fd_to_add[word]
+        else:
+            total_fd[word] = fd_to_add[word]
+    return total_fd
+
+
+def extract_related_keyword(merged_seq, most_common = 50, ngram = 1):
+    '''
+    Find a dominantly appearing keywords when the stock is in rise and in fall.
+
+    :param merged_seq: a pandas Series where each element is a list of news and a label.
+    :return: keywords that appear dominantly for each label.
+    '''
+    increase = []
+    decrease = []
+    unnecessary = stopwords.words('english')
+    unnecessary.append("’")
+    total_fd_increased, total_fd_decreased = nltk.FreqDist({}), nltk.FreqDist({})
+    increased_total, decreased_total = 0, 0
+    for pair in merged_seq:
+        news = pair[0]
+        labeled = pair[1]
+        if labeled == [1,0,0]:
+            for txt in news:
+                txt = txt.translate(str.maketrans('', '', string.punctuation))
+                tokenized = word_tokenize(txt)
+                filtered_tokenized = [word.lower() for word in tokenized if word.lower() not in unnecessary and not word.lower().isdecimal()]
+                if ngram == 1:
+                    fd = nltk.FreqDist(filtered_tokenized)
+                elif ngram == 2:
+                    fd = nltk.FreqDist(nltk.bigrams(filtered_tokenized))
+                else: continue
+                for word in fd:
+                    fd[word] /= float(fd.N())
+                total_fd_increased = addup_fd(total_fd_increased, fd)
+                increased_total += 1
+        elif labeled == [0,0,1]:
+            for txt in news:
+                txt = txt.translate(str.maketrans('', '', string.punctuation))
+                tokenized = word_tokenize(txt)
+                filtered_tokenized = [word.lower() for word in tokenized if word.lower() not in unnecessary and not word.lower().isdecimal()]
+                if ngram == 1:
+                    fd = nltk.FreqDist(filtered_tokenized)
+                elif ngram == 2:
+                    fd = nltk.FreqDist(nltk.bigrams(filtered_tokenized))
+                else: continue
+                for word in fd:
+                    fd[word] /= float(fd.N())
+                total_fd_decreased = addup_fd(total_fd_decreased, fd)
+                decreased_total += 1
+        else:
+            continue
+    for word in total_fd_increased:
+        total_fd_increased[word] /= float(increased_total)
+    for word in total_fd_decreased:
+        total_fd_decreased[word] /= float(decreased_total)
+    inc_list, dec_list = total_fd_increased.most_common(most_common), total_fd_decreased.most_common(most_common)
+    inc_word_list = [pair[0] for pair in inc_list]
+    dec_word_list = [pair[0] for pair in dec_list]
+    inc_filtered = [pair for pair in inc_list if pair[0] not in dec_word_list]
+    dec_filtered = [pair for pair in dec_list if pair[0] not in inc_word_list]
+    # inc dec 각각 제일 흔한 것 50개 잡고 겹치지 않도록 겹치는 것들은 전부 제거, 그리고 남은 안 겹치는 것들 중에 freq별로 정리?
+    return inc_filtered, dec_filtered # lists of high frequency words in increase and decrease [(word, frequency) *]
+
+inc, dec = extract_related_keyword(merged_data)
+#print(inc)
+#print(dec)
+
+print([pair[0] for pair in inc])
+print([pair[0] for pair in dec])
+
+
+
+'''아래 data가 training & testing에 이용될 수 있는 featureset!'''
+
 
 featurelist = ['mentioned vs not mentioned',        # 회사 언급된 문장 수 / 언급되지 않은 문장 수
                'company mentioned polarity scores', # 회사 언급된 문장의 polarity score 평균
@@ -91,12 +193,9 @@ def featurizer(texts, company_name):
 
     return feature
 
-#growth_rate = stock_data.market_stock_growth_interval('AAPL', 'IXIC', 14, '2010-01-01') # for feature vs rate
-#merged_data = data_collector.merge_price_text(text, growth_rate) # for feature vs rate #(text, label)
-merged_data = data_collector.merge_price_text(text, label)
 
-'''아래 data가 training & testing에 이용될 수 있는 featureset!'''
-featureset = [(featurizer(pair[0], 'apple'), tuple(pair[1])) for pair in merged_data] # data to train
+
+#featureset = [(featurizer(pair[0], 'apple'), tuple(pair[1])) for pair in merged_data] # data to train
 
 
 #featureset = [(featurizer(pair[0], 'apple'), float(pair[1])) for pair in merged_data] # for feature vs rate
@@ -128,7 +227,7 @@ def feature_vs_growth_rate(feature_set):
 
 
 
-
+'''
 ## Featureset이 주가의 경향성을 어떻게 반영하는지
 
 x1, x2, x3, x4, x5, y = featureset_plotdata(featureset)
@@ -169,7 +268,7 @@ plt.ylabel('increase or decrease')
 
 plt.show()
 
-
+'''
 
 
 ##################################################################################################
